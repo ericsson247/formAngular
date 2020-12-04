e128b409cd5268d056e310481c9465c26c072459
Eric Yohanna Mshelmbula <ericyohanna247@gmail.com>
2020-12-04 13:59:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
